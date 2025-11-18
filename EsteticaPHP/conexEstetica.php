5fe3838a8ea2af37400b9f5b1d223cedf45b9ab2
Joafs7 <fabiansantillan2000@gmail.com>
2025-11-18 20:17:25 -0300

    Mod

--- EsteticaPHP/conexEstetica.php
+++
@@ -1,6 +0,0 @@
-<?php
-function conectarDB(){
-    $conex=mysqli_connect("localhost","root","","esteticadb");
-    return $conex;
-}
-?>
--- EsteticaPHP/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-session_destroy(); // Destruye la sesión
-header("Location: index.php"); // Redirige al inicio
-exit();
-?>
